841d212e9fdc593b46202759fc7d597ab4098549
Slawomir Matuszak <59056051+slawomirmatuszak@users.noreply.github.com>
2020-02-16 22:45:52 +0100

    script final

--- run_analysis.R
+++ run_analysis.R
@@ -7,6 +7,7 @@ library(dplyr)
 # download and unzip dataset
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 path <- getwd()
+# creating separate data folder is not required, but I personally prefer to have data in a data directory
 if (!file.exists("data")) { 
  dir.create("data") 
 }
@@ -60,3 +61,4 @@ data_tidy_final <- data_tidy %>%
 # save the output
 write.table(data_tidy_final, "data_tidy_final.txt", row.name=FALSE)
 write.csv(data_tidy_final, "data_tidy_final.csv")
+# csv file just in case, as it is more common format for data. 
